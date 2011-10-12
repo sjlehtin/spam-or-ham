de4eb95e5a911ac39c4c117d08c661069d7e63f5
Sami J. Lehtinen <sjl@iki.fi>
2011-10-12 20:33:28 +0300

    Added graph output to "tree.dot".  Image can be generated by "dot -Teps tree.dot > tree.eps", for example.

--- decisiontree.py
+++ decisiontree.py
@@ -3,10 +3,14 @@ import sys
 import math
 
 class TreeNode(object):
+    cur_id = 1
+
     def __init__(self):
         self.left = None
         self.right = None
         self.ratio = 0
+        self.id = TreeNode.cur_id
+        TreeNode.cur_id += 1
 
 DEBUG = True
 def verbose(msg):
@@ -107,6 +111,52 @@ def generate_tree(data, theta, min_row_ratio):
 
     return tree_node(data)
 
+
+def dump_tree(dt):
+
+    fp = open("tree.dot", "w")
+    cur_id = 1
+    nodes = {}
+    def output_header():
+        print >> fp, "digraph decisiontree {"
+
+    def output_trailer():
+         print >> fp, "}"
+
+    def output_node(node):
+        print >> fp, "n%s;\n" % (node.id)
+
+    def output_edge(n1, n2):
+        print >> fp, "n%s -> n%s;\n" % (n1.id, n2.id)
+
+    def dump_node(cur):
+        if not cur.left:
+            return
+
+        assert cur.right and cur.left
+
+        output_node(cur.left)
+        output_node(cur.right)
+        output_edge(cur, cur.left)
+        output_edge(cur, cur.right)
+
+        dump_node(cur.left)
+        dump_node(cur.right)
+
+    output_header()
+    output_node(dt)
+    dump_node(dt)
+    output_trailer()
+    fp.close()
+
+def classify(dt, data):
+    classified = numpy.zeros((numpy.size(data, 0), 3), dtype=object)
+
+
+
+    return classified
+
+
 if __name__ == "__main__":
 
     data = numpy.loadtxt("data.txt", comments="%", dtype=int,
@@ -116,4 +166,10 @@ if __name__ == "__main__":
     training_data = data[0:1000, :]
     indices = numpy.random.permutation(numpy.size(data, 1) - 2)
     training_data[:,2:] = training_data[:,2:][:,indices]
-    generate_tree(training_data, 0.2, 0.05)
+    dt = generate_tree(training_data, 0.3, 0.05)
+
+    dump_tree(dt)
+
+    classified = classify(dt, data[1000:, :])
+
+    numpy.savetxt("classified.txt", classified, fmt=["%d", "%d", "%f"])
