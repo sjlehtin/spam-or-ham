bb84818b8228eb18797542539d27d8613197094c
Bomanson Jori <jbomanso@cc.hut.fi>
2011-10-12 14:55:39 +0300

    Updated.

--- naive-bayes/classify_with_naive_bayes.m
+++ naive-bayes/classify_with_naive_bayes.m
@@ -1,14 +1,21 @@
 function g=classify_with_naive_bayes(unknown, p_hat, p_c)
 
-  p_hat(p_hat == 0) = 0.00000000000000001;
+  p_hat(p_hat == 0) = 0.00000000000000000000000000001;
 
   g1 = sum( unknown * log(p_hat(1, :)') ...
 	  + (1 - unknown) * log(1 - p_hat(1, :)'), 2) ...
-	  + log(p_c(1, :))
+	  + log(p_c(1, :));
 
   g2 = sum( unknown * log(p_hat(2, :)') ...
 	  + (1 - unknown) * log(1 - p_hat(2, :)'), 2) ...
-	  + log(p_c(2, :))
+	  + log(p_c(2, :));
 
+  p1 = exp(g1);
+  p2 = exp(g2);
+  s = p1 + p2;
+  p1 = p1 ./ s;
+  p2 = p2 ./ s;
+
+  %g = p1;
   g = g1 > g2;
 end
--- naive-bayes/main.m
+++ naive-bayes/main.m
@@ -1,4 +1,4 @@
-function classified = main()
+function [test_data_r, train_classified] = main()
 
   SPAM_ROWS = 500;
   NON_SPAM_ROWS = 500;
@@ -11,6 +11,30 @@ function classified = main()
 
   data = data(:, not(sum(known_data, 1) == 0));
 
+  %%%
+  known_data = data(1 : KNOWN_ROW_COUNT, :);
+  idx = randperm(KNOWN_ROW_COUNT)';
+  TRAIN_ROWS = 700;
+  TEST_ROWS = KNOWN_ROW_COUNT - TRAIN_ROWS;
+
+  t = idx(1:TRAIN_ROWS);
+  train_data = [t <= 500, known_data(t, :)];
+
+  t = idx(TRAIN_ROWS + 1 : KNOWN_ROW_COUNT);
+  test_data = known_data(t, :);
+  test_data_r = t <= 500;
+
+  t = train_data(:, 2:end);
+  train_data_spam = t(train_data(:, 1) == 1);
+  train_data_nonspam = t(train_data(:, 1) == 0);
+
+  train_p_hat = train_naive_bayes(train_data_spam, train_data_nonspam);
+  train_classified = classify_with_naive_bayes(test_data, train_p_hat, ...
+	  [sum(train_data(:, 1)) / TRAIN_ROWS;
+	  sum(1 - train_data(:, 1)) / TRAIN_ROWS]);
+
+  return
+  %%%
 
   spam_data = data(1 : SPAM_ROWS, 3 : end);
   nonspam_data = data(SPAM_ROWS + 1 : KNOWN_ROW_COUNT, 3 : end);
